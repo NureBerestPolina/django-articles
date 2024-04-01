be82bd775204dfa631df4b619a751dd9710b1a2b
Polina Berest <pamapolya@gmail.com>
2024-04-01 23:34:47 +0300

    Added: css file, articles output

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Article
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    articles = Article.objects.all()
+    return render(request, 'main/index.html', {'title': 'Home', 'articles': articles})
